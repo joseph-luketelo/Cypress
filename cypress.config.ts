d04b83f67c090620dec3f98018953ab4d594498f
Joseph Luketelo <joseph.luketelo@gmail.com>
2024-02-05 16:51:57 +0300

    add cypress.config.ts file

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  viewportWidth: 1080,
+  viewportHeight: 720,
+  chromeWebSecurity: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'https://example.cypress.io/',
+    experimentalStudio: true
+  }
+}
